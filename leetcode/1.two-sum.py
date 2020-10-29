01e1f38d5a5af41f9588b0a64c5463a8f098fb43
ZIJIE on Windows <z.zijie@outlook.com>
2020-10-28 21:51:10 -0500

    Problem1-Accepted

---
+++ leetcode/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+# @lc code=end
+
