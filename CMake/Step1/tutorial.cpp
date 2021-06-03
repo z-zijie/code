d62e738242313c771c796d799eab95b6da898014
ZIJIE ZHANG <z.zijie@outlook.com>
2021-06-03 20:01:09 +0800

    CMake Build

---
+++ CMake/Step1/tutorial.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  double x;
+  cout << "ENTER ANY NUMBER:";
+  cin >> x;
+  cout << "SQUARE ROOT OF " << x << " is " << sqrt(x) << endl;
+  return 0;
+}
