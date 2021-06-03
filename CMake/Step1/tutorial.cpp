1d8c32d1213d9b5411b650bde1aadb4f7ef63788
ZIJIE ZHANG <z.zijie@outlook.com>
2021-06-03 20:43:52 +0800

    Feature 1(Version Number)

--- CMake/Step1/tutorial.cpp
+++ CMake/Step1/tutorial.cpp
@@ -1,8 +1,16 @@
+#include <TutorialConfig.h>
 #include <cmath>
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    // report version
+    cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+         << Tutorial_VERSION_MINOR << endl;
+    cout << "Usage: " << argv[0] << " number" << endl;
+    return 1;
+  }
   double x = atof(argv[1]);
   cout << "SQUARE ROOT OF " << x << " is " << sqrt(x) << endl;
   return 0;
--- CMake/Step1_build/CMakeFiles/Tutorial.dir/tutorial.cpp.o.d
+++ CMake/Step1_build/CMakeFiles/Tutorial.dir/tutorial.cpp.o.d
@@ -1,6 +1,8 @@
 CMakeFiles/Tutorial.dir/tutorial.cpp.o: \
  /home/zijie/Documents/code/CMake/Step1/tutorial.cpp \
- /usr/include/stdc-predef.h /usr/include/c++/10.2.0/cmath \
+ /usr/include/stdc-predef.h \
+ /home/zijie/Documents/code/CMake/Step1_build/TutorialConfig.h \
+ /usr/include/c++/10.2.0/cmath \
  /usr/include/c++/10.2.0/x86_64-pc-linux-gnu/bits/c++config.h \
  /usr/include/c++/10.2.0/x86_64-pc-linux-gnu/bits/os_defines.h \
  /usr/include/features.h /usr/include/sys/cdefs.h \
